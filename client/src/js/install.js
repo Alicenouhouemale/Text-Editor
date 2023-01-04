98f46805bac9eefa4bdecdf8cd1ff3cd1648a17f
Alice Nouhouemale <alicenouhouemale@gmail.com>
2023-01-04 14:19:08 -0800

    update

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,17 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener("beforeinstallprompt", (event) => {
+  // Storing the triggered events
+  window.deferredPrompt = event;
+
+  // To remove the hidden class from the button.
+  butInstall.classList.toggle("hidden", false);
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener("click", async () => {});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener("appinstalled", (event) => {});
